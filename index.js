ade4614cf7aadfcc6017bc38690ea1b783a26290
tlaudahl <travis.laudahl@gmail.com>
2019-10-28 16:59:00 -0500

    CRUD Operations done - MVP Complete

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ server.use(express.json());
 
 server.post('/api/users', (req, res) => {
     const newUser = req.body;
-    db.insert(newUser)
-    .then(user => {
-        if(newUser.name && newUser.bio) {
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({ errorMessage: 'Please provide name and bio for the user' })
+    } else {
+        db.insert(newUser)
+        .then(user => {
             res.status(201).json(user)
-        } else {
-            res.status(400).json({ errorMessage: 'Please provide name and bio for the user' })
-        }
-    })
-    .catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database' }))
+        })
+        .catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database' }))
+    }
 })
 
 server.get('/api/users', (req, res) => {
@@ -45,6 +45,40 @@ server.get('/api/users/:id', (req, res) => {
         res.status(500).json({ error: 'The user information could not be retrieved.'})
     })
 })
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(user => {
+        if(user) {
+            return res.status(200).json(user)
+        } else {
+            return res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(err => res.status(500).json({ error: "The user could not be removed" }))
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body;
+    const id = req.params.id;
+    if(!name || !bio) {
+        res.status(400).json({ errorMessage: 'Please provide a name and bio for the user' })
+    } else {
+        db.update(id, req.body)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: 'The user with the specified ID does not exist'})
+            }
+        })
+        .catch(err => res.status(404).json({ errorMessage: 'The user information could not be modified'}))
+    }
+})
+
+
 
 
 
